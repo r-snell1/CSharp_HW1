4e81f6171f77ceb85e4c8a842b7b7d2c4706b1cf
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-03-15 19:09:08 -0500

    Add files via upload

---
+++ Chapter1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Chapter1
+{
+
+    class Chapter1
+    {
+        
+        public static void Main()
+        {
+
+            PersonalInfo();
+            Lyrics();
+
+        }//end main
+        static void PersonalInfo()
+        {
+            string name = "Ryan Snell";
+            string birthday = "06 Dec 1982";
+            string cellPhoneNumber = "4041441-3523";
+            
+            Console.WriteLine(name + " " + birthday + " " + cellPhoneNumber);
+            
+        }//end method
+
+        static void Lyrics()
+        {
+            
+            Console.WriteLine("Do you have the time, to listen to me whine?");
+            Console.WriteLine("about nothing and everything all at once.");
+            Console.WriteLine("I am one of those, melodramtic fools,");
+            Console.WriteLine("neurotic to the bone.");
+            
+        }
+    }//end class
+    
+}//end namespace
+
