20f39e92270e241af0ef34d72e93e378f9034bc0
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-03-22 15:35:01 -0400

    Add files via upload

---
+++ Chapter5Exercises.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Chapter5Exercises
+{
+    
+    internal class Chapter5Exercises
+    {
+        
+        public static void Main(string[] args)
+        {
+            
+            homeSales();
+            perfect();
+            countVowels();
+            guessingGame();
+            
+        }//end main
+        
+        /* Danielle, Edward, and Francis are three salespeople at Holiday Homes. Write an
+        application named HomeSales that prompts the user for a salesperson initial (D, E,
+        or F ). Either uppercase or lowercase initials are valid. While the user does not type Z,
+        continue by prompting for the amount of a sale. Issue an error message for any invalid
+        initials entered. Keep a running total of the amounts sold by each salesperson. After
+        the user types Z or z for an initial, display each salesperson’s total, a grand total for all
+        sales, and the name of the salesperson with the highest total. */
+        static void homeSales()
+        {
+            
+            
+            
+        }//end method
+        
+        /* Write an application named Perfect that displays every perfect number from 1
+        through 10,000. A number is perfect if it equals the sum of all the smaller positive
+        integers that divide evenly into it. For example, 6 is perfect because 1, 2, and 3 divide
+        evenly into it and their sum is 6. */
+        static void perfect()
+        {
+            
+            
+            
+        }//end method
+        
+        /* Write a program named CountVowels that accepts a phrase from the user and counts
+        the number of vowels in the phrase. For this exercise, count both uppercase and
+        lowercase vowels, but do not consider y to be a vowel. */
+        static void countVowels()
+        {
+            
+            
+            
+        }//end method
+        
+        /* Modify the GuessingGame2 program to create a program called GuessingGame3
+        in which the player is criticized for making a “dumb” guess. For example, if the player
+        guesses that the random number is 4 and is told that the guess is too low, and then the
+        player subsequently makes a guess lower than 4, display a message that the user should
+        have known not to make such a low guess. */
+        static void guessingGame()
+        {
+            
+            
+            
+        }//end method
+        
+    }//end class
+    
+}//end namespace
