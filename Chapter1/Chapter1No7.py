05adacce8e2268ad35df842d4ba6b186ab2647b8
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-04-08 11:01:12 -0400

    Delete Chapter1 directory

--- Chapter1/Chapter1No7.py
+++
@@ -1,3 +0,0 @@
-name = str(input("Enter your name: "))
-age = int(input("Enter your age: "))
-print("Your name is {} and your age is {}".format(name, age))
--- Chapter1/Chapter1no4.py
+++
@@ -1,4 +0,0 @@
-width = int(input("Enter a width: "))
-height = int(input("Enter a height: "))
-area = width * height
-print(f'The area is {area} square units')
